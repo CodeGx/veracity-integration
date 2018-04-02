23659ba0d92efa1df4dbe4df66ee8382eeba4a75
Xin David Guo <Xin.David.Guo@dnvgl.com>
2018-04-02 13:54:19 +0800

    Added authentication schemes handling

---
+++ VeracityIntegrationDemo/VeracityIntegrationDemo/AzureAdB2C/MyDnvglExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace VeracityIntegrationDemo.AzureAdB2C
+{
+    public static class MyDnvglExtension
+    {
+        public static string Name(this ClaimsPrincipal user)
+        {
+            return user.FindFirst("name")?.Value;
+        }
+
+        public static string MyDnvglId(this ClaimsPrincipal user)
+        {
+            return user.FindFirst("userId")?.Value;
+        }
+
+        public static string DnvglAccountName(this ClaimsPrincipal user)
+        {
+            return user.FindFirst("dnvglAccountName")?.Value;
+        }
+
+        public static string Email(this ClaimsPrincipal user)
+        {
+            return user.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress")?.Value;
+        }
+    }
+}
